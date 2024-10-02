8b2364a936bc35311c439aa7cb45a101e930a49f
S-Vilka <sergei.vilka@metropolia.com>
2024-10-02 13:47:21 +0300

    Starter code for Coding Marathon 3

---
+++ frontend/src/components/JobListings.jsx
@@ -0,0 +1,35 @@
+// import JobListing from "./JobListing";
+
+// const JobListings = ({ jobs }) => {
+  
+//   return (
+//     <div className="job-list">
+//       {jobs.map((job) => (
+//         <JobListing key={job.id} job={job} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default JobListings;
+
+import { Link } from "react-router-dom";
+
+const JobListings = ({ jobs }) => {
+  return (
+    <div className="job-list">
+      {jobs.map((job) => (
+
+        <div className="job-preview" key={job.id}>
+          <Link to={`/jobs/${job.id}`}>
+            <h2>{job.title}</h2>
+          </Link>
+          <p>Type: {job.type}</p>
+          <p>Company: {job.company.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default JobListings;
